8ce79fe6bee3edd987e0bb4cdeeaa0a66dd42cf3
arjuna <arjunabangka12@gmail.com>
2023-05-25 13:28:02 +0700

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","index.html"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgba(38, 32, 33, 1)",
+        colorText : "rgba(243, 90, 90, 1)"
+      },
+      height: {
+        '55': '13.8rem',
+      },
+      fontSize: {
+        s : ['10px','14px']
+      }
+    },
   },
   plugins: [],
-}
-
+};
