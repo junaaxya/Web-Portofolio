8c3630c9d9c1490166a846e9b00a9a7483c49e19
arjuna <arjunabangka12@gmail.com>
2023-05-27 14:53:51 +0700

    change bg and text

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         s : ['10px','14px']
       },
       maxWidth: {
-        '17': '17rem',
+        '17': '20rem',
       },
       aspectRatio: {
         '4/3': '4 / 3',
