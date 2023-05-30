04619fd492e2e803f5ca96e3cf443cf4be447812
arjuna <arjunabangka12@gmail.com>
2023-05-30 09:41:19 +0700

    add box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    container:{
-      
-    },
+    container: {},
     extend: {
       colors: {
         primary: "rgba(38, 32, 33, 1)",
-        colorText : "rgba(243, 90, 90, 1)",
-        linear1:"rgba(18, 18, 24, 1)",
-        linear2:"#101c30"
+        colorText: "rgba(243, 90, 90, 1)",
+        linear1: "rgba(18, 18, 24, 1)",
+        linear2: "#101c30",
       },
-      padding:{
-        7: '7px'
+      fontFamily: {
+        opensans: ["Josefin Sans"],
+        pangolin: ["Pangolin"],
+      },
+      padding: {
+        7: "7px",
       },
       height: {
-        '55': '13.5rem',
+        55: "13.5rem",
       },
       fontSize: {
-        s : ['10px','14px']
+        s: ["10px", "14px"],
       },
       maxWidth: {
-        '17': '22.7rem',
+        17: "22.7rem",
       },
       aspectRatio: {
-        '4/3': '4 / 3',
+        "4/3": "4 / 3",
       },
     },
   },
